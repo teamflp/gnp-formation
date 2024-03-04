0aa90b0d69671d577ed7353a36f581d6d1557a73
teamflp <teamflp@yahoo.it>
2024-03-04 17:03:48 +0100

    first commit for gnp-formation

---
+++ src/Entity/About.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AboutRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AboutRepository::class)]
+class About
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
